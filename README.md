0ae933f815c0af7a5080c8d58ab48962b0f09b9c
vmrfreitas <vmrfreitas@inf.ufrgs.br>
2018-12-19 01:28:07 -0200

    Prim is working

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cad_trab_final
+
 Trabalho final da disciplina de CAD - UFRGS
+
+### Undirected Weighted Graph
+
+Abaixo se encontra uma visualização do exemplo utilizado para verificar a corretude dos algoritmos
+Em vermelho está a árvore de extensão mínima para o determinado exemplo.
+
+
+```
+// undirected weighted graph
+// format-> (node1) (node2) (distance)
+A B 2
+A C 3
+A D 3
+B C 4
+B E 3
+C E 1
+C D 5
+C F 6
+D F 7
+E F 8
+F G 9
+```
+
+
+![image](/graph/graph_undirected.png)
--- src/prim.py
+++ src/prim.py
@@ -1,18 +1,19 @@
 import re
 import operator
-
+import sys
+import copy
 
 class Edge():
-    def __init__(self, node1, node2, distance):
-        self.node1 = node1
-        self.node2 = node2
-        self.distance = distance
+	def __init__(self, node1, node2, distance):
+		self.node1 = node1
+		self.node2 = node2
+		self.distance = distance
 
 class Node():
 	def __init__(self, node, distance, pred):
-        self.node = node
-        self.distance = distance
-        self.pred = pred
+		self.node = node
+		self.distance = distance
+		self.pred = pred
 
 
 def prim(graph):
@@ -20,57 +21,74 @@ def prim(graph):
 
 	# initialize the distances
 	for edge in graph:
-		if edge.node1 not in nodes:
+		found1 = False
+		found2 = False
+		for node in nodes:
+			if edge.node1 == node.node:
+				found1 = True
+			elif edge.node2 == node.node:
+				found2 = True
+
+		if not found1:
 			node = Node(edge.node1, sys.maxsize, None)
 			nodes.append(node)
-		if edge.node2 not in nodes:
+		if not found2:
 			node = Node(edge.node2, sys.maxsize, None)
 			nodes.append(node)
 
 	# first node is starting node
 	nodes[0].distance = 0 
+	nodes_list = copy.deepcopy(nodes)
 
-	while not nodes:
-		curr_node = nodes.pop(0)
+	while nodes_list:
 
-		for edge in graph:
+		curr_node = nodes_list.pop(0)
+
+		for edge in graph: 
 			found = False
 			if edge.node1 == curr_node.node:
 				next_node = edge.node2
+				new_distance = edge.distance
 				found = True
-			else if edge.node2 == curr_node.node:
+			elif edge.node2 == curr_node.node:
 				next_node = edge.node1
-				found = True
+				new_distance = edge.distance
+				found =True
 
 			if found:
-				new_distance = edge.distance
+				for node_l in nodes_list:
+					if node_l.node == next_node:
+						if new_distance<node_l.distance:
+
+							for node in nodes:
+								if node.node == node_l.node:
+									node.pred = curr_node.node
+									node.distance = new_distance
+									node_l.distance = new_distance
+									break
+							break
+
+				nodes_list = sorted(nodes_list, key=operator.attrgetter('distance'))
+
+	return nodes
 
 
-			
-			for x in test_list:
-			    if x.value == value:
-			        print "i found it!"
-			        break			
 
-
-	nodes = sorted(nodes, key=operator.attrgetter('distance'))
-
-	
-
-
-
-f = open("../graph/graph_undirected.txt","r")
+f = open("../graph/graph_undirected2.txt","r")
 
 graph = []
 for line in f:
 	if not re.match("//", line):
 		node1, node2, distance = line.split(" ")
 		edge = Edge(node1, node2, distance)
+		edge.distance = int(edge.distance)
 		graph.append(edge)
 
-prim(graph)
+result = prim(graph)
 
+print("Resulting minimum spanning tree:")
 
-for edge in graph:
-	attrs = vars(edge)
-	print(', '.join("%s: %s" % item for item in attrs.items()))
+result.pop(0)
+
+for node in result:
+	print("node1: %s, node2: %s, distance: %s" % (node.pred, node.node, node.distance))
